854368b29f89ee086f4e25e9deadc862457adb9a
Fathiah23 <akinsfee@gmail.com>
2023-03-16 12:06:16 -0700

    7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - container for block statement
+ * fputs - printout function
+ * Return: print 0 for success
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			stdout) ;
+	return (0);
+}
