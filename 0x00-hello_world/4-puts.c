23c3946cf39704214f2870e59e4f1f5ecb3051fb
Fathiah23 <akinsfee@gmail.com>
2023-03-16 06:20:57 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - contains no argument
+ * puts function - to print out a text followed by a newline
+ * return - print 0 to indicate program end
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
