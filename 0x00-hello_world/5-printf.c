2bcc15adff764cd2e6cef6d629fcb857dfe5f7a1
Fathiah23 <akinsfee@gmail.com>
2023-03-16 07:27:15 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** main - The container for the statement block
+ * printf - The print out function
+ * return - returns the 0 to indicate end of program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
