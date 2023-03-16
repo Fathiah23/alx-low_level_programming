e0397a27113ed2f4f00975e7ca66c958e6679819
Fathiah23 <akinsfee@gmail.com>
2023-03-16 12:44:31 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - container for block statement
  * fputs - printout function
- * Return: print 0 for success
+ * Return: print 1 for success
  */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			stdout);
-	return (0);
+	return (1);
 }
