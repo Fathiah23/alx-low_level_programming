12268eb4f71efbd3e7b8db9849c08e5aea721adb
Fathiah23 <akinsfee@gmail.com>
2023-03-17 10:48:56 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main container
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	}
+	return (0);
+}
