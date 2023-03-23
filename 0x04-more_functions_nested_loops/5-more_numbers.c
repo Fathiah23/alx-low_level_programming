3ff516158ab1bfb1265a5897bdb2a5e045d303f3
Fathiah23 <akinsfee@gmail.com>
2023-03-23 14:18:53 -0700

    4-5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,13 @@ void more_numbers(void)
 	int i, j;
 
 	for (i = 1; i <= 10; i++)
+	{
 		for (j = 0; j <= 14; j++)
+		{
 			if (j >= 10)
-			{
 				_putchar(j);
-				_putchar(j % 10 * '0');
-			}
-	_putchar('\n');
+			_putchar(j % 10 * '0');
+		_putchar('\n');
+		}
+	}
 }
