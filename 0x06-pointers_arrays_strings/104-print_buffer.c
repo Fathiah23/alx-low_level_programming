7a50691c1053565eadd0f99a6f6d1bdbc9bbfc47
Fathiah23 <akinsfee@gmail.com>
2023-03-31 13:15:38 -0700

    4

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_buffer - function name
@@ -18,7 +19,7 @@ void print_buffer(char *b, int size)
 	{
 		j = size - 0 < 10 ? size - 0 : 10;
 		printf("%08x;", o);
-		for (i = 0, i < 10; i++)
+		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
 				printf("%02x", *(b + o + i));
