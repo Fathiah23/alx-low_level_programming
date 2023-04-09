536e6500fe63718e00efe36f0c95d4812f2ccd45
Fathiah23 <akinsfee@gmail.com>
2023-04-09 09:38:47 -0700

    2-3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - main function body
+ * _atoi - called fxn
+ * @s: a pointer to a string
+ * Return: value of x
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	int num = 0;
+
+	while (s[y] != '\0')
+		y++;
+	while (i < y && z == 0)
+	{
+		if (s[i] == '-')
+			++j;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = s[i] - '0';
+			if (j % 2)
+				num = -num;
+			x = x * 10 + num;
+			z = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			z = 0;
+		}
+		i++;
+	}
+		if (z == 0)
+			return (0);
+		return (x);
+}
+/**
+ * main - main fxn body
  * @argc: argument count
  * @argv: argument variable
  * Return: 0 for success
